0c87b780528cf43cadf2ee954b9fe6b1a8348ea4
Dicky <dickysetyo99@gmail.com>
2026-05-07 20:53:21 +0700

    feature modulus

--- index.ts
+++ index.ts
@@ -2,3 +2,7 @@ let n1: number = 20;
 let n2: number = 30;
 
 console.log(`Application is running`);
+
+// Dicky
+const modulus: number = n1 % n2;
+console.log(`Hasil modulus ${n1} % ${n2} = ${modulus}`);
