52ef33ce15925db0d867f1ed2d8c42b3366c8114
Yulius Chandra Gunawan <yuliuscg@gmail.com>
2026-05-07 20:52:09 +0700

    feat: add increment value to index.js file

--- index.ts
+++ index.ts
@@ -2,3 +2,7 @@ let n1: number = 20;
 let n2: number = 30;
 
 console.log(`Application is running`);
+
+let n3: number = 10;
+const increment: number = ++n3;
+console.log(`Hasil increment ${n3} = ${increment}`);
