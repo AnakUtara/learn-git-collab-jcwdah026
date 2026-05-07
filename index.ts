4ffaa4e3bf19e019837b4d9071fbba8708976900
rajakucing <rajakucing@192.168.1.13>
2026-05-07 21:58:58 +0800

    multiplication

--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ let n1: number = 20;
 let n2: number = 30;
 
 console.log(`Application is running`);
+
+const multiplication: number = n1 * n2;
+console.log(`Hasil perkalian ${n1} * ${n2} = ${multiplication}`);
