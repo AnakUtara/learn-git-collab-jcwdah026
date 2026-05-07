bdc1f405cff243e87ecb56fb2689762ca65282dc
gallantgibran <gallantgibran024@gmail.com>
2026-05-07 20:54:01 +0700

    feat(diff): add difference feature to index.ts

--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ let n1: number = 20;
 let n2: number = 30;
 
 console.log(`Application is running`);
+
+const difference: number = n1 - n2;
+console.log(`Hasil pengurangan ${n1} - ${n2} = ${difference}`);
